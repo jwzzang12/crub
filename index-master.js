30ab80a9f6267b26bf54420260f8d832a13d2372
unknown <moonbb12@gmail.com>
2022-08-18 16:18:34 +0900

    second commit

--- index-master.js
+++ index-master.js
@@ -1,10 +1,10 @@
-const express = require("express");
 const path = require("path");
+const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
-
-//mongodb 관련 모듈
+// mongodb관련 모듈
 const MongoClient = require("mongodb").MongoClient;
+
 let db = null;
 MongoClient.connect(process.env.MONGO_URL, { useUnifiedTopology: true }, (err, client) => {
   console.log("연결");
@@ -13,18 +13,13 @@ MongoClient.connect(process.env.MONGO_URL, { useUnifiedTopology: true }, (err, c
   }
   db = client.db("crudapp");
 });
-
 app.use(express.urlencoded({ extended: false }));
-//post 방식으로 데이터를 넘겨 받기 위해서 필요한 것
 app.set("view engine", "ejs");
-app.set("port", process.env.PORT || 8099);
-const PORT = app.get("port");
-
 app.get("/", (req, res) => {
   res.render("index");
 });
 app.get("/write", (req, res) => {
-  // res.sendFile(path.join(__dirname, "public/html/write.html"));
+  //res.sendFile(path.join(__dirname, "public/html/write.html"));
   res.render("write");
 });
 app.post("/add", (req, res) => {
@@ -32,7 +27,8 @@ app.post("/add", (req, res) => {
     const total = result.totalPost;
     const subject = req.body.subject;
     const contents = req.body.contents;
-    //id 대신 name으로 받기 때문에 name이 중요한 것!
+    console.log(subject);
+    console.log(contents);
     const insertData = {
       no: total + 1,
       subject: subject,
@@ -43,32 +39,24 @@ app.post("/add", (req, res) => {
         if (err) {
           console.log(err);
         }
-        console.log("잘들어갔음");
-        res.send(`<script>alert("글이 입력되었습니다");location.href="/list"</script>`);
+        //console.log("잘 들어갔음");
+        res.send(`<script>alert("글이 입력되었습니다."); location.href="/list"</script>`);
       });
     });
   });
 
-  // res.sendFile(path.join(__dirname, "public/html/result.html"));
-
-  //node에서는 브라우저 스크립트가 작동하지 않기 때문에 알림창과 같은 브라우저 스크립트를
-  //사용하고 싶으면 이렇게 하는 수 밖에 없음
-
-  // res.send, res.redirect 동시에 쓸 수 없음
-  // 이유는 send를 통해 데이터 보내고 나면 종료
-
-  // 1. db 접속
-  // 2. 데이터 밀어넣기
+  //res.sendFile(path.join(__dirname, "public/html/result.html"));
+  //res.redirect("/list");
 });
-//데이터를 밀어넣을떄
+// 불면확한 데이터 처리할때 noSql (몽고db) 사용
 app.get("/list", (req, res) => {
   //crud에서 데이터 받아보기
   db.collection("crud")
     .find()
     .toArray((err, result) => {
       console.log(result);
-      // res.json(result); // front가 알아서 처리해서 가져다 쓰기
-      res.render("list", { list: result, title: "테스트dd" }); //페이지를 내가 만들어서 보내주기
+      //res.send(); res.json();res.sendFile();
+      res.render("list", { list: result, title: "테스트용입니다." }); //   페이지 내가 만들어서 보내주기
     });
 });
 app.get("/detail/:no", (req, res) => {
@@ -79,11 +67,12 @@ app.get("/detail/:no", (req, res) => {
       console.log(err);
     }
     if (result) {
+      console.log(result);
       res.render("detail", { subject: result.subject, contents: result.contents });
     }
   });
+  //res.render("detail");
 });
-//받은 데이터를 단순히 뿌릴 때
-app.listen(PORT, () => {
-  console.log(`${PORT}에서 서버 대기 중`);
+app.listen(8099, () => {
+  console.log("8099에서 서버 대기중");
 });
